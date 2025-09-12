f5754a46220a9b3b2b4d708bffd31441d1da4588
Przemyslaw Pietras <przemyslawp94@gmail.com>
2025-09-12 13:40:00 +0200

    feat: list tasks for multiple projects

--- cmd/ttrack/list.go
+++ cmd/ttrack/list.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/destag/ttrack/internal/config"
 	"github.com/destag/ttrack/internal/jira"
+	"github.com/destag/ttrack/internal/tasks"
 )
 
 var cmdList = &cli.Command{
@@ -37,25 +38,27 @@ func runList(ctx context.Context, cmd *cli.Command) error {
 		return cli.Exit("project not found", 1)
 	}
 
-	if len(proj.Tasks) != 1 {
-		return cli.Exit("project has multiple tasks", 1)
+	var ts []*tasks.Task
+
+	for _, task := range proj.Tasks {
+		if task.Type != "jira" {
+			continue
+		}
+
+		query := task.Query
+		if query == "" {
+			continue
+		}
+
+		out, err := jc.ListTasks(query)
+		if err != nil {
+			return cli.Exit(err.Error(), 1)
+		}
+
+		ts = append(ts, out...)
 	}
 
-	if proj.Tasks[0].Type != "jira" {
-		return cli.Exit("only jira projects are supported", 1)
-	}
-
-	query := proj.Tasks[0].Query
-	if query == "" {
-		return cli.Exit("project has no query", 1)
-	}
-
-	tasks, err := jc.ListTasks(query)
-	if err != nil {
-		return cli.Exit(err.Error(), 1)
-	}
-
-	for _, task := range tasks {
+	for _, task := range ts {
 		fmt.Printf("%s %s\n", color.BlueString(task.ID), task.Description)
 	}
 
